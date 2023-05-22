df66f9ca37ec95036cd4aa35efdad8ebfaad51e3
George Meskhoradze <116169423+George-Meskhoradze@users.noreply.github.com>
2023-05-22 21:48:28 +0400

    Please enter the commit message for your changes. Lines starting

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Todo from './Todo'
-
+//test
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Todo />
